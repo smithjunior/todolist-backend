a80fac35b29da9647c109935c055de4b0b021203
Smith Júnior <smith.junior@icloud.com>
2019-09-29 15:40:36 -0300

    Added mongo connection with mongoose

--- src/server.js
+++ src/server.js
@@ -4,6 +4,12 @@ const cors = require('cors')
 const app = express()
 const server = require('http').Server(app)
 
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGO_URL_CONNECTION, {
+  useNewUrlParser: true
+})
+
 app.use(cors())
 app.use(express.json())
 
